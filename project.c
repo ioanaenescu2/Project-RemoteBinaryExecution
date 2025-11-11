9bc9e4569eb8521828f53edcb4f4a6c4fa277d1d
ioanaenescu2 <151065755+ioanaenescu2@users.noreply.github.com>
2025-11-11 21:18:33 +0200

    Incepere implementare interfata

---
+++ project.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 4096;
+
+void handle_connect(char *ip, char *port, char *user)
+{
+    printf("conectare");
+}
+void handle_submit(char *file_path)
+{
+    printf("sumit");
+}
+void handle_status(char *job_id)
+{
+    printf("status");
+}
+void handle_fetch(char *job_id)
+{
+    printf("fetch");
+}
+void handle_exit()
+{
+    printf("Disconnected. Goodbye!\n");
+    
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+    char line[512];
+    printf("Remote Executor Client \n\n");
+
+    while(1)
+    {
+        printf("> ");
+        if((fgets(line, sizeof(line), stdin)) == NULL)
+        {
+            perror("citire");
+            exit(EXIT_FAILURE);
+        }
+
+        line[strcspn(line, "\n")]=0;
+        //printf("%s \n", line);
+
+        char *command=strtok(line, " ");
+        if(!command)
+        {
+            continue;
+        }
+
+        if((strcmp(command, "connect")) == 0)
+        {
+            char *ip=strtok(NULL, " ");
+            char *port=strtok(NULL, " ");
+            char *user=strtok(NULL, " ");
+            if(ip && port && user)
+            {
+                handle_connect(ip, port, user);
+            }
+            else
+            {
+                printf("Use: <IP> <PORT> <USER_NAME> \n");
+            }
+        }
+        else if((strcmp(command, "submit")) == 0)
+        {
+            char *file_path=strtok(NULL, " ");
+            if(file_path)
+            {
+                handle_submit(file_path);
+            }
+            else
+            {
+                printf("Use: <FILE_PATH> \n");
+            }
+        }
+        else if((strcmp(command, "status")) == 0)
+        {
+            char *job_id=strtok(NULL, " ");
+            if(job_id)
+            {
+                handle_status(job_id);
+            }
+            else
+            {
+                printf("Use: <JOB_ID> \n");
+            }
+        }
+        else if((strcmp(command, "fetch")) == 0)
+        {
+            char *job_id=strtok(NULL, " ");
+            if(job_id)
+            {
+                handle_fetch(job_id);
+            }
+            else
+            {
+                printf("Use: <JOB_ID> \n");
+            }
+        }
+        else if((strcmp(command, "exit")) == 0)
+        {
+            handle_exit();
+        }
+        else
+        {
+            printf("Unknown command.\n");
+        }
+    }
+
+    return(0);
+}
